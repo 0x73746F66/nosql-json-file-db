232398dddb44b77a9eb7d893f3a6b8ead8eb64cb
Joshua Parker <joshmac3@icloud.com>
2016-10-07 16:00:46 -0400

    Update db.lib.php
    Formatted class, added missing property `$key`, added methods `orde`r & `reset`, and move instructions to readme.

--- db.lib.php
+++ db.lib.php
@@ -1,58 +1,57 @@
 <?php
+
 class db
 {
-	private static $path;
-	private $table;
-	private $data = array();
-	public $result = false;
-	private $updated = array();
-	private $deleted = array();
 
-	public function __construct( $path = null )
-	{
-        !empty( $path ) && is_string( $path ) ? $this->path = (string)$path : $this->path = '.' ;        
+    private static $path;
+    private $table;
+    private $data = array();
+    public $result = false;
+    private $updated = array();
+    private $deleted = array();
+    private $key;	
+
+    public function __construct($path = null)
+    {
+        !empty($path) && is_string($path) ? $this->path = (string) $path : $this->path = '.';
         return $this;
     }
-    public function select( $table , $where = null )
+
+    public function select($table, $where = null)
     {
         $this->table = $table;
         $this->data = array();
         $this->result = false;
-        $key = false;
-        if ( is_readable( $this->path . "/" . $this->table . ".json" ) ) 
-        {
-            $table_arr = json_decode( file_get_contents( $this->path . "/" . $this->table . ".json" ), true ) ;
-            if ( empty( $where ) ) {
-                $this->data = $table_arr ;
+        $this->key = false;
+        if (is_readable($this->path . "/" . $this->table . ".json")) {
+            $table_arr = json_decode(file_get_contents($this->path . "/" . $this->table . ".json"), true);
+            if (empty($where)) {
+                $this->data = $table_arr;
                 $this->result = true;
-            } elseif ( is_string( $where ) ) {
-                $this->key = $where ;
-                foreach( $table_arr as $row ) 
-                {
-                    if ( array_key_exists( $this->key , $row ) ) {
-                        $this->data[] = $row ;
-                        $this->result = true ;
+            } elseif (is_string($where)) {
+                $this->key = $where;
+                foreach ($table_arr as $row) {
+                    if (array_key_exists($this->key, $row)) {
+                        $this->data[] = $row;
+                        $this->result = true;
                     }
                 }
-            } elseif ( is_array( $where ) ) {
-                $keys = array_keys( $where );
-                
-                foreach( $table_arr as $row ) 
-                {
-                    if ( array_key_exists( $keys[0] , $row ) ) {
-                        $matches_arr[] = $row ;
+            } elseif (is_array($where)) {
+                $keys = array_keys($where);
+
+                foreach ($table_arr as $row) {
+                    if (array_key_exists($keys[0], $row)) {
+                        $matches_arr[] = $row;
                     }
                 }
-                for( $i = 0; $i < count( $keys ); ++$i ) 
-                {
-                    foreach($matches_arr as $match)
-                    {
-                        if ( array_key_exists( $keys[$i], $match ) && $match[$keys[$i]] == $where[$keys[$i]] ) {
+                for ($i = 0; $i < count($keys); ++$i) {
+                    foreach ($matches_arr as $match) {
+                        if (array_key_exists($keys[$i], $match) && $match[$keys[$i]] == $where[$keys[$i]]) {
                             $results[] = $match;
                         }
                     }
                 }
-                if ( count( $results ) > 0 ) {
+                if (count($results) > 0) {
                     $this->data = $results;
                     $this->result = true;
                 }
@@ -60,227 +59,198 @@ class db
         }
         return $this;
     }
-    public function id( $pk )
+
+    public function id($pk)
     {
-		//after a select full table, search for a record with pk matching $pk to return
-		foreach( $this->data as $row )
-			if ( array_key_exists( 'pk' , $row ) && $row['pk'] === $pk ) 
-				return $row ;
-		return false ;
+        //after a select full table, search for a record with pk matching $pk to return
+        foreach ($this->data as $row)
+            if (array_key_exists('pk', $row) && $row['pk'] === $pk)
+                return $row;
+        return false;
     }
-    public function get( $method = 'select' )
+
+    public function get($method = 'select')
     {
-        switch ($method)
-        {
+        switch ($method) {
             case "select":
-            return $this->result ? $this->data : $this->result ;
-            break;
+                return $this->result ? $this->data : $this->result;
+                break;
             case "update":
-            return $this->updated ;
-            break;
+                return $this->updated;
+                break;
             case "delete":
-            return $this->deleted ;
-            break;
+                return $this->deleted;
+                break;
         }
     }
-    public function insert( $table , $record )
+
+    /**
+     * Filter the results by one of more fields.
+     * 
+     * @since 1.0.0
+     * @param string $field Field in a particular table.
+     * @param bool $reverse Sorted order: desc, or asc.
+     * @return mixed
+     */
+    public function order($field, $reverse = false)
     {
-        if ( is_readable( $this->path . "/" . $table . ".json" ) ) 
-        {
-            $data = json_decode( file_get_contents( $this->path . "/" . $table . ".json" ), true ) ;
-			if ( array_key_exists( 'pk' , $record ) ) {
-				foreach( $data as $row )
-				array_key_exists( 'pk' , $row ) && $record['pk'] != $row['pk'] ? true : $return = false ;
-				if ( $return === false ) return false ;
-			} else {
-				$this->key = 'pk' ;
-				$this->data = $data ;
-				$max = $this->distinct() ;
-				$record['pk'] = max( $max ) + 1 ;
-				unset( $this->key ) ;
-				$this->data = array() ;
-			}
-        } else {
-			$data = array();
-			if ( !array_key_exists( 'pk' , $record ) ) {
-				$record['pk'] = 1;
-			}
-		}
-        $data[] = $record;
-        return file_put_contents( $this->path . "/" . $table . ".json", json_encode( $data ) ) ? true : false ;
+        $hash = [];
+
+        $records = $this->result ? $this->data : $this->result;
+
+        foreach ($records as $key => $record) {
+            $hash[$record[$field] . $key] = $record;
+        }
+
+        ($reverse) ? krsort($hash) : ksort($hash);
+
+        $records = [];
+
+        foreach ($hash as $record) {
+            $records [] = $record;
+        }
+
+        return $records;
     }
-    private function replace( &$value , $key )
+
+    public function insert($table, $record)
     {
-        if( $key == $this->find )
+        if (is_readable($this->path . "/" . $table . ".json")) {
+            $data = json_decode(file_get_contents($this->path . "/" . $table . ".json"), true);
+            if (array_key_exists('pk', $record)) {
+                foreach ($data as $row)
+                    array_key_exists('pk', $row) && $record['pk'] != $row['pk'] ? true : $return = false;
+                if ($return === false)
+                    return false;
+            } else {
+                $this->key = 'pk';
+                $this->data = $data;
+                $max = $this->distinct();
+                $record['pk'] = max($max) + 1;
+                unset($this->key);
+                $this->data = array();
+            }
+        } else {
+            $data = array();
+            if (!array_key_exists('pk', $record)) {
+                $record['pk'] = 1;
+            }
+        }
+        $data[] = $record;
+        return file_put_contents($this->path . "/" . $table . ".json", json_encode($data)) ? true : false;
+    }
+
+    private function replace(&$value, $key)
+    {
+        if ($key == $this->find)
             $value = $this->replace;
     }
-    public function update( $where )
+
+    public function update($where)
     {
         $this->updated = $this->data;
-        if ( is_array( $where ) )
-        foreach ( $where as $key => $value )
-        {
-            $this->find = $key;
-            $this->replace = $value;
-            array_walk_recursive( $this->updated , array( $this , 'replace' ) ) ;
-        }
-        return $this ;
+        if (is_array($where))
+            foreach ($where as $key => $value) {
+                $this->find = $key;
+                $this->replace = $value;
+                array_walk_recursive($this->updated, array($this, 'replace'));
+            }
+        return $this;
     }
+
     public function rollback()
     {
-        unset( $this->table );
+        unset($this->table);
         $this->data = array();
         $this->result = false;
         $this->updated = array();
         $this->deleted = array();
     }
+
     public function commit()
     {
-        if ( is_readable( $this->path . "/" . $this->table . ".json" ) ) 
-        {
-            if ( !empty( $this->updated ) && is_array( $this->updated ) && count( $this->updated ) > 0 ) {
-                $table_arr = json_decode( file_get_contents( $this->path . "/" . $this->table . ".json" ), true ) ;
-                foreach( $table_arr as $row )
-                {
+        if (is_readable($this->path . "/" . $this->table . ".json")) {
+            if (!empty($this->updated) && is_array($this->updated) && count($this->updated) > 0) {
+                $table_arr = json_decode(file_get_contents($this->path . "/" . $this->table . ".json"), true);
+                foreach ($table_arr as $row) {
                     $match = false;
-                    foreach( $this->data as $selected_record )
-                    {
-                        if ( $row == $selected_record )
-                        {
+                    foreach ($this->data as $selected_record) {
+                        if ($row == $selected_record) {
                             $match = true;
                         }
                     }
                     if ($match == false) {
-                        $new_records[] = $row ;
+                        $new_records[] = $row;
                     }
                 }
-                foreach( $this->updated as $updated_record )
-                {
-                    $new_records[] = $updated_record ;
+                foreach ($this->updated as $updated_record) {
+                    $new_records[] = $updated_record;
                 }
-            } elseif ( !empty( $this->deleted ) && is_array( $this->deleted ) && count( $this->deleted ) > 0 ) {
-                $new_records = $this->deleted ;
-            } else { echo "failed here"; }
-            unset( $this->updated ) ;
-            unset( $this->deleted ) ;
-            
-            if ( is_array( $new_records ) && count( $new_records ) > 0 )
-            return file_put_contents( $this->path . "/" . $this->table . ".json", json_encode( $new_records ) ) ? true : false ;
-            else return false ;
-        } else return false ;
+            } elseif (!empty($this->deleted) && is_array($this->deleted) && count($this->deleted) > 0) {
+                $new_records = $this->deleted;
+            } else {
+                echo "failed here";
+            }
+            unset($this->updated);
+            unset($this->deleted);
+
+            if (is_array($new_records) && count($new_records) > 0)
+                return file_put_contents($this->path . "/" . $this->table . ".json", json_encode($new_records)) ? true : false;
+            else
+                return false;
+        } else
+            return false;
     }
-    public function delete( $table , $where )
+
+    public function delete($table, $where)
     {
-        $this->result = false ;
-        $this->table = $table ;
-        if ( is_readable( $this->path . "/" . $this->table . ".json" ) ) 
-        {
-            $table_arr = json_decode( file_get_contents( $this->path . "/" . $this->table . ".json" ), true ) ;
-            $this->data = $table_arr ;
-            $this->result = true ;
-            
-            $keys = array_keys( $where );
-            for( $i = 0; $i < count( $table_arr ); ++$i )
-            {
-                $next = true ;
-                while ( $next === true )
-                {
-                    foreach ( $where as $key => $value )
-                    {
-                        if ( array_key_exists( $key, $table_arr[$i] ) && $table_arr[$i][$key] == $value ) {
+        $this->result = false;
+        $this->table = $table;
+        if (is_readable($this->path . "/" . $this->table . ".json")) {
+            $table_arr = json_decode(file_get_contents($this->path . "/" . $this->table . ".json"), true);
+            $this->data = $table_arr;
+            $this->result = true;
+
+            $keys = array_keys($where);
+            for ($i = 0; $i < count($table_arr); ++$i) {
+                $next = true;
+                while ($next === true) {
+                    foreach ($where as $key => $value) {
+                        if (array_key_exists($key, $table_arr[$i]) && $table_arr[$i][$key] == $value) {
                             $next = true;
                         } else {
-                            break 2 ;
+                            break 2;
                         }
                     }
                     unset($table_arr[$i]);
-                    $next = false ;
+                    $next = false;
                 }
             }
 
-            $this->deleted = $table_arr ;
-            return $this ;
-        } else return false ;
+            $this->deleted = $table_arr;
+            return $this;
+        } else
+            return false;
     }
+
     public function distinct()
     {
-        if ( isset( $this->key ) && !empty( $this->key ) && is_string( $this->key ) && is_array( $this->data ) )
-		{
-			foreach( $this->data as $row )
-			$result[] = $row[$this->key] ;
-			return array_unique( $result ) ;
-		} else return false ;
+        if (isset($this->key) && !empty($this->key) && is_string($this->key) && is_array($this->data)) {
+            foreach ($this->data as $row)
+                $result[] = $row[$this->key];
+            return array_unique($result);
+        } else
+            return false;
+    }
+    
+    public function reset($table)
+    {
+        $this->table = $table;
+        $file = $this->path . "/" . $this->table . ".json";
+        if (is_readable($file)) {
+            $fp = fopen($file, 'w');
+            ftruncate($fp, 0);
+            fclose($fp);
+        }
     }
 }
-/* 
-//################ Usage ################
-// create an instance and set the path to the database, defaults to this directory.
-$db = new db( 'db' ) ;
-
-// prepare a new user record
-$newuser['username'] = 'chrisdlangton' ;
-$newuser['email'] = 'chris@codewiz.biz' ;
-$newuser['active'] = true ;
-//or
-$newuser = array(
-            'username' => 'chrisdlangton'
-            ,'email' => 'chris@codewiz.biz'
-            ,'active' => true
-            ) ;
-// insert a new user (will also create the 'table')
-$db->insert( 'users' , $newuser ) ;
-
-// query all records in 'table'
-$db->select( 'users' )->get() ;
-
-// query all records in 'table' that have a 'column' matching x but the value of column x may be anything as long as column x exists
-$db->select( 'users' , 'email' )->get() ;
-
-// query all records in 'table' that have a 'column' with values matching an array of columns and values.
-$db->select( 'users', array( 'username'=>'chrisdlangton' , 'active'=>true , 'email'=>'chris@codewiz.biz' ) )->get() ;
-
-// prepare an update, use get() to view the results of the select
-$db->select( 'users' , 'email' )->update( array( 'username'=>'testing' ) )->get() ;
-
-// prepare an update, use get( 'update' ) to view the results of the update before committing
-$db->select( 'users' , 'email' )->update( array( 'username'=>'testing' ) )->get( 'update' ) ;
-
-// prepare an update, use commit() to commit the changes to the db
-$db->select( 'users' , 'email' )->update( array( 'username'=>'testing' ) )->commit() ;
-// or
-$db->select( 'users' , 'email' ) ;
-$db->update( array( 'username'=>'testing' ) ) ;
-$db->commit() ;
-
-// prepare an update with multiple changes, use commit() to commit the changes to the db
-$db->select( 'users' , 'email' )->update( array( 'username'=>'testing','active'=>false ) )->commit() ;
-// or
-$db->select( 'users' , 'email' ) ;
-$db->update( array( 'username'=>'testing','active'=>false ) ) ;
-$db->commit() ;
-
-// select by primary key id
-$db->select( 'users' )->id( 5 ) ;
-
-// select all distict values in a table by column name
-$db->select( 'users', 'email' )->distinct() ;
-
-// prepare a delete, use get() to view the original table data
-$db->delete( 'users' , array( 'username'=>'testing' ) )->get() ;
-
-// prepare a delete, use get( 'delete' ) to view the new table data before commiting
-$db->delete( 'users' , array( 'username'=>'testing' ) )->get( 'delete' ) ;
-
-// prepare a delete, use commit() to commit the changes to the db
-$db->delete( 'users' , array( 'username'=>'testing' ) )->commit() ;
-// or
-$db->delete( 'users' , array( 'username'=>'testing' ) ) ;
-$db->commit() ;
-
-// prepare an update with multiple changes, use commit() to commit the changes to the db
-$db->delete( 'users' , array( 'active'=>true , 'username'=>'chrisdlangton' ) )->commit() ;
-// or
-$db->delete( 'users' , array( 'active'=>true , 'username'=>'chrisdlangton' ) ) ;
-$db->commit() ;
-
- */
